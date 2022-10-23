8c5c898c0b4fc27e21a0f70cb8302771bbc1e29e
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2022-10-23 19:14:58 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,37 +1,13 @@
-# NuPrGvtres 
+# NuPrGv3 
 # Copyright (c) Odycloud.
 
 #!python
 #cython: language_level=3
 import sys
-sys.path.append('/opt/Odycloud/NumPre_Gv3')
-sys.path.append('/opt/Odycloud/NumPre_Gv3/plugin')
+sys.path.insert(1,'/opt/.Odycloud/NumPre_Gv3')
+sys.path.insert(1,'/opt/.Odycloud/NumPre_Gv3/plugin')
 
-def classFactory(iface):
-    """Load QGISPlugin class.
-    """
-###    from NumPre_Gv3.plugin.constants import PLUGIN_NAME
-###    from NumPre_Gv3.plugin.ui.helpers import WaitDialog
-    import mainPlugin
-  
-    title = iface.mainWindow().windowTitle()
-    new_title = title.replace('QGIS', 'Gv3 Graphical Interface')
-    iface.mainWindow().setWindowTitle(new_title)
-    vector_menu = iface.vectorMenu()
-    raster_menu = iface.rasterMenu()
-#    mesh_menu = iface.meshMenu()
-    database_menu = iface.databaseMenu()
-    web_menu = iface.webMenu()
-#    processing_menu = iface.processingMenu()
-    menubar = vector_menu.parentWidget()
-    menubar.removeAction(vector_menu.menuAction())
-    menubar.removeAction(raster_menu.menuAction())
-    menubar.removeAction(database_menu.menuAction())
-#    menubar.removeAction(mesh_menu.menuAction())
-    menubar.removeAction(web_menu.menuAction())
-#    menubar.removeAction(processing_menu.menuAction())
-#    menubar.addAction(dummy_menu)
-    return QGISPlugin(iface)
+from NumPre_Gv3 import mainPlugin 
 
 def dummy_menu():
     pass
