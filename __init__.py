0db73d3d521f545009d0512be0d4ea432ab645b4
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2022-10-24 19:15:58 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,10 +4,10 @@
 #!python
 #cython: language_level=3
 import sys
-sys.path.insert(1,'/opt/.Odycloud/NumPre_Gv3')
-sys.path.insert(1,'/opt/.Odycloud/NumPre_Gv3/plugin')
+sys.path.insert(1,'/opt/.Odycloud/Ody_NumPre')
+sys.path.insert(1,'/opt/.Odycloud/Ody_NumPre/GUI')
 
-from NumPre_Gv3 import mainPlugin 
+from Ody_NumPre import mainPlugin 
 
 def dummy_menu():
     pass
