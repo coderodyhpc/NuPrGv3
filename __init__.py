70c31510c886ad73af9c5481f70f126e780ddc96
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2022-10-24 19:43:29 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ import sys
 sys.path.insert(1,'/opt/.Odycloud/Ody_NumPre')
 sys.path.insert(1,'/opt/.Odycloud/Ody_NumPre/GUI')
 
-from Ody_NumPre import mainPlugin 
+from mainPlugin import QGISPlugin  
 
 def dummy_menu():
     pass
